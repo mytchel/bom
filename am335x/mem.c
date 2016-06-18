0686786d01133b8c0fe25c3ae2122d1702230b30
Mytchel Hammond <mytchel@openmailbox.org>
2016-06-19 08:58:57 +1200

    Memory mapping working

--- am335x/mem.c
+++ am335x/mem.c
@@ -10,70 +10,8 @@ extern void *_kernel_heap_end;
 
 static uint8_t *next;
 
-static uint32_t mmu_ttb[4096];
-
-void
-mmu_init(void)
-{
-	int i;
-	
-	kprintf("mmu_init\n");	
-	
-	for (i = 0; i < 4096; i++)
-		mmu_ttb[i] = L1_FAULT;
-	
-	asm(
-		"ldr r0, =mmu_ttb		\n"
-		"mcr p15, 0, r0, c2, c0, 0	\n"
-	);
-}
-
-
-void
-mmu_enable(void)
-{
-	kprintf("mmu_enable\n");
-	asm(
-		/* Invalidate tlb */
-		"mcr p15, 0, r1, c8, c7, 0	\n"
-		/* Enable mmu */
-		"mrc p15, 0, r0, c1, c0, 0	\n"
-		"orr r0, r0, #1			\n"
-		"mcr p15, 0, r0, c1, c0, 0	\n"
-	);
-}
-
-void
-mmu_disable(void)
-{
-	asm(
-		"mrc p15, 0, r0, c1, c0, 0	\n"
-		"bic r0, r0, #1			\n"
-		"mcr p15, 0, r0, c1, c0, 0	\n"
-	);
-}
-
-void
-mmu_map_page(void *phys, void *vert, size_t npages, uint8_t perms)
-{
-	int i;
-	uint32_t virt_a, phys_a;
-	
-	virt_a = (uint32_t) vert;
-	phys_a = (uint32_t) phys;
-	
-	for (i = 0; i < npages; i++) {
-		id = virt_a >> 20;
-		
-		mmu_ttb[id] = phys_a
-		
-		virt_a += PAGE_SIZE;
-		phys_a += PAGE_SIZE;
-	}
-	
-	/* Invalidate tlb */	
-	asm("mcr p15, 0, r1, c8, c7, 0");
-}
+static uint32_t mmu_ttb[4096] __attribute__((__aligned__(16* 1024)));
+//static uint32_t l2[4096][256] __attribute__((__aligned__(1024)));
 
 void
 memory_init(void)
@@ -87,17 +25,116 @@ memory_init(void)
 	kprintf("heap_end   = 0x%h\n", (uint32_t) &_kernel_heap_end);
 	
 	next = (uint8_t *) &_kernel_heap_start;
-	
+
+	kprintf("mmu_ttb = 0x%h\n", mmu_ttb);
+		
 	mmu_init();
 
 	/* Map kernel memory */	
-	kbounds = PAGE_ALIGN((uint32_t) &_kernel_bin_start) >> PAGE_SHIFT;
-	kbounde = PAGE_ALIGN((uint32_t) &_kernel_bin_end) >> PAGE_SHIFT;
+	kbounds = PAGE_ALIGN((uint32_t) &_kernel_bin_start - 0x80000000) >> PAGE_SHIFT;
+	kbounde = PAGE_ALIGN((uint32_t) &_kernel_bin_end -   0x80000000) >> PAGE_SHIFT;
 
+	kprintf("kbounds s: 0x%h e: 0x%h npages: %i \n", kbounds, kbounde, kbounde - kbounds);
+
+	/* Direct map memory map, for now. */
+	mmu_map_page((void *) 0x40000000, (void *) 0x40000000,
+		164, MMU_AP_RW_NO);
+
+	/* Map kernel space */
 	mmu_map_page(&_kernel_bin_start, &_kernel_bin_start, 
 		kbounde - kbounds, MMU_AP_RW_NO);
 
 	mmu_enable();
+}
+
+void
+mmu_init(void)
+{
+	int i;
+	
+	kprintf("mmu_init\n");	
+	
+	for (i = 0; i < 4096; i++)
+		mmu_ttb[i] = L1_FAULT;
+	
+	asm(
+		/* Set ttb */
+		"ldr r1, =mmu_ttb		\n"
+		"mcr p15, 0, r1, c2, c0, 0	\n"
+		/* Disable domains ? */
+		"mov r1, #3			\n"
+		"mcr p15, 0, r1, c3, c0, 0	\n"
+	);
+}
+
+
+void
+mmu_enable(void)
+{
+	kprintf("mmu_enable\n");
+	asm(
+		/* Invalidate tlb */
+		"mcr p15, 0, r1, c8, c7, 0	\n"
+		/* Enable mmu */
+		"mrc p15, 0, r1, c1, c0, 0	\n"
+		"orr r1, r1, #1			\n"
+		"mcr p15, 0, r1, c1, c0, 0	\n"
+	);
+}
+
+void
+mmu_disable(void)
+{
+	asm(
+		"mrc p15, 0, r1, c1, c0, 0	\n"
+		"bic r1, r1, #1			\n"
+		"mcr p15, 0, r1, c1, c0, 0	\n"
+	);
+}
+
+void
+mmu_map_page(void *phys, void *vert, size_t npages, uint8_t perms)
+{
+	int i, id;
+	uint32_t virt_a, phys_a;
+	
+	virt_a = (uint32_t) vert;
+	phys_a = (uint32_t) phys;
+	
+	kprintf("mmu_map_page 0x%h size 0x%h\n", (uint32_t) phys, npages);
+	
+	for (i = 0; i < npages; i++) {
+		id = virt_a >> 20;
+	
+		kprintf("mmu map page id 0x%h ... 0x%h to 0x%h\n", id, virt_a, phys_a);
+			
+		/* If L1 for address is not set, point it to the L2 table. */
+
+		mmu_ttb[id] = (phys_a) | (3 << 10) | L1_SECTION;
+		
+		kprintf("mmu_ttb[%i] = 0b%b\n", id, mmu_ttb[id]);
+/*
+			mmu_ttb[id] = (uint32_t) &(l2[id]) | L1_COARSE;
+			for (j = 0; j < 256; j++)
+				l2[id][j] = L2_FAULT;
+				*/
+		
+		/* Pointer to coarse page table. */
+		/*
+		page = (uint32_t *) (mmu_ttb[id] & ~((1 << 10) - 1));
+		
+		kprintf("get l2 id\n");
+		id = (virt_a & 0xff000) >> 12;
+		
+		kprintf("set\n");
+		page[id] = (phys_a & 0xfffff000) | L2_SMALL | (0xff<<4);
+*/
+		virt_a += PAGE_SIZE;
+		phys_a += PAGE_SIZE;
+	}
+	
+	/* Invalidate tlb */	
+	asm("mcr p15, 0, r1, c8, c7, 0");
 }
 
 void *
--- am335x/mem.h
+++ am335x/mem.h
@@ -1,7 +1,7 @@
 #ifndef __MEM
 #define __MEM
 
-#define PAGE_SHIFT 	12
+#define PAGE_SHIFT 	20
 #define PAGE_SIZE	(1UL << PAGE_SHIFT)
 #define PAGE_MASK	(~(PAGE_SIZE - 1))
 #define PAGE_ALIGN(x) 	(((x) + PAGE_SIZE - 1) & PAGE_MASK)
@@ -16,7 +16,7 @@
 #define L1_SECTION	2
 #define L1_FINE		3
 
-#define L2_FAUlT	0
+#define L2_FAULT	0
 #define L2_LARGE	1
 #define L2_SMALL	2
 #define L2_TINY		3
@@ -36,6 +36,10 @@ mmu_enable(void);
 void
 mmu_disable(void);
 
+void
+mmu_map_page(void *phys, void *vert,
+	size_t npages, uint8_t perms);
+	
 void
 memory_init(void);
 
